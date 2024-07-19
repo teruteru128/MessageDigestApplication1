0923ed3e9253dc23dba0d3b4a5a6e34350430c30
Teruteru <teruterubouzu1024@gmail.com>
2024-07-19 18:54:20 +0900

    C++からC#に変更しますた

---
+++ MessageDigestApplication1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Org.BouncyCastle.Crypto.Digests;
+using System.Text;
+
+var data = Encoding.UTF8.GetBytes("うんち！ｗ");
+var digest = new RipeMD160Digest();
+var result = new byte[digest.GetDigestSize()];
+digest.BlockUpdate(data, 0, data.Length);
+digest.DoFinal(result, 0);
+
+Console.WriteLine(string.Join("", result.Select(x => $"{x:x2}")));
+
