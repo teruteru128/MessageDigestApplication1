9962b3102b8d5441911f69734dc085dfcb167941
Teruteru <teruterubouzu1024@gmail.com>
2024-07-19 23:56:53 +0900

    Add .Net Framework 4.8 Project

---
+++ MessageDigestApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageDigestApplication2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1) {
+                Console.Error.WriteLine("コマンドライン引数が不足しています！");
+                Environment.Exit(1);
+            }
+            var data = Encoding.UTF8.GetBytes(args[0]);
+            var digest = new RipeMD160Digest();
+            var result = new byte[digest.GetDigestSize()];
+            digest.BlockUpdate(data, 0, data.Length);
+            digest.DoFinal(result, 0);
+
+            Console.WriteLine(string.Join("", result.Select(x => $"{x:x2}")));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MessageDigestApplication1
+
+## Message Digest Application 1
+
+.NET 8.0 Application
+
+## Message Digest Application 2
+
+.Net Framework 4.8 Application
